4d3514b1225c8abc38417ea6c69950c65dcf1710
Moritz Kaufmann <moritz.kaufmann@tum.de>
2017-02-04 13:42:44 +0100

    bench_local.sh now returns error if argument missing.

--- bench_local.sh
+++ bench_local.sh
@@ -3,5 +3,11 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
 
+if [ "$#" -ne 1 ]; then
+    echo "Error: Invalid number of parameters"
+    echo "Usage: ./bench_local.sh <workload_file>"
+    exit 1
+fi
+
 ./compile.sh
 build/Release/bencher/bencher "$1" ./run.sh
