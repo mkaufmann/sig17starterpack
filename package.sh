9f5a05ddee264db4e786bd1b71e24fc009b6616a
Moritz Kaufmann <moritz.kaufmann@tum.de>
2017-02-06 18:53:48 +0100

    Ignore more files when creating package.

--- package.sh
+++ package.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR"
 
-/bin/tar --exclude='*.load' --exclude='*.data' --exclude='.*' --exclude='build' -czf  submission.tar.gz *
+/bin/tar --exclude='*.load' --exclude='*.data' --exclude='.*' --exclude='build' --exclude='submission.tar.gz' --exclude='*ngrams.txt' --exclude='test-harness' -czf  submission.tar.gz *
